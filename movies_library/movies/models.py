61719bae53c8c73a7c6c7edc2256fd2a0df3c92e
diegolinkk <diegolinkk@gmail.com>
2021-09-06 17:58:56 -0300

    configuração do admin, pluralização e migração inicial

--- movies_library/movies/models.py
+++ movies_library/movies/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=20,)
+
+    class Meta:
+        verbose_name_plural = "Categories" #indica o nome correto do plural no admin
+    
+    def __str__(self): #lista de forma mais natural a propriedade do objeto nas listas
+        return self.category_name
+
+class Movie(models.Model):
+    movie_name = models.CharField(max_length=50)
+    publish_date = models.DateField()
+    category = models.ForeignKey(Category, on_delete = models.SET_NULL,null=True)
+
+    def __str__(self):
+        return self.movie_name
