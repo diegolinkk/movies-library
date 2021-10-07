ffcb5378d9b7e68bbdc39a83999d968514be6da6
Diego Oliveira da Silva <diegolinkk@gmail.com>
2021-10-06 21:56:12 -0300

    movie add by post form ok

--- movies_library/movies/views.py
+++ movies_library/movies/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Movie
+from django.shortcuts import render,HttpResponseRedirect
+from django.urls import reverse
+from .models import Movie,Category
 
 # Create your views here.
 
@@ -9,4 +10,17 @@ def index(request):
     return render(request,'movies/index.html',{'movies': movies})
 
 def add_movie(request):
+    if request.method =='POST':
+        m = Movie()
+        c = Category.objects.get(id=request.POST['category'])
+
+        m.movie_name = request.POST['movie_name']
+        m.publish_date = request.POST['publish_date']
+        m.category = c
+
+        m.save()
+
+        return HttpResponseRedirect(reverse('movies:index'))
+
+
     return render(request,'movies/adicionar.html')
